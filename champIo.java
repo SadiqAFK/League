2e812eb8b962b0a041781b8691ef8104516e02cb
sprinklrr <59484067+sprinklrr@users.noreply.github.com>
2020-01-08 14:14:53 -0500

    Add files via upload

--- champIo.java
+++ champIo.java
@@ -4,5 +4,33 @@ import java.util.*;
 public class champIo {
   public static void main(String[] args) {
     
+    String csvFile = "champList.csv";
+    BufferedReader br = null;
+    String line = "";
+    String cvsSplitBy = ",";
+    
+    try {
+      
+      br = new BufferedReader(new FileReader(csvFile));
+      while ((line = br.readLine()) != null) {
+        // use comma as separator
+        String[] row = line.split(cvsSplitBy);
+        System.out.println(Arrays.toString(row));
+      }
+      
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (br != null) {
+        try {
+          br.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+    }
+    
   }
 }
