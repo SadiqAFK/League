caf2a0a4ba28c0838568aff0da1c22986ada92bd
Nathan Chung <nathan_chung1@yahoo.ca>
2020-01-11 15:39:52 -0500

    added mana cost, armor/mr calcs

--- Ability.java
+++ Ability.java
@@ -3,24 +3,21 @@ class Ability extends Champion{
   private String abilityName;
   private double manaCost;
   
-  //This array willl store the total magic damage and totla attack damage of the ability
-  private double[] damage=new double[2];
+  //This array willl store the total magic damage and total attack damage of the ability
+  // Slot 0 is ad, slot 1 is md
+  private double[] damage = new double[2];
   
   //counter may be used for abilities which deal more damage when used in sucession
   private int counter;
-
   private double boost;
-  private double amplify;
-    
-  //private boolean combo;
-    
-  Ability(String name, double attack, double magic){
-    
+  private double amplify;  
+  
+  Ability(String name, double attack, double magic, double mana){
     abilityName=name;
+    manaCost = mana;
     damage[0]=attack;
     damage[1]=magic;
   }
-  
   
   //Getters
   public String getName(){
@@ -34,7 +31,10 @@ class Ability extends Champion{
   public double getBaseAttack(){
     return damage[0];
   }
-    
+  
+  public double getManaCost() {
+    return manaCost;
+  }
   
   //Setters
   public void setBaseMagic(double baseMagic){
@@ -44,26 +44,41 @@ class Ability extends Champion{
   public void setBaseAttack(double baseAttack){
     damage[0]=baseAttack;
   }
-    
+  
   //Methods
   
-  public double[] attack(Champion champion){
+  public double[] attack(Champion champion, Champion enemyChamp, Ability ability){
     
-    double championAttack = champion.getAtkDmg();
-    double championMagic = champion.getMagicDmg();
-      
-    if(getBaseAttack() < 0){
+    // Champion physical damage is champions ad + ability base ad
+    double championAttack = champion.getAtkDmg() + ability.getBaseAttack();
+    
+    // Champion magical damage is champions magic d + ability base magic d
+    double championMagic = champion.getMagicDmg() + ability.getBaseMagic();
+    
+    // If champions attack is less than 0, make it 1
+    if(champion.getAtkDmg() + ability.getBaseAttack() < 0){
       championAttack = 1;
     }
     
-    else if(getBaseMagic()<0){
+    else if(champion.getAtkDmg() + ability.getBaseAttack() < 0){
       championMagic = 1;
     }
     
-    damage[0]+=championAttack;
-    damage[1]+=championMagic;
+    // Armor calculation, finds ad damage multiplier factor
+    double percentArmor = 100 / (100 + enemyChamp.getArmor());
     
-    return damage;
+    // Magic res calculation, finds m damage multiplier factor
+    double percentRes = 100 / (100 + enemyChamp.getMagicRes());
+    
+    // Calculates damage factors into both types of damage
+    championAttack = championAttack * percentArmor;
+    championMagic = championMagic * percentRes;
+    
+    // Adds champion attacks to damage array
+    damage[0] = championAttack;
+    damage[1] = championMagic;
+    
+    return damage; // Returns damage given to enemy champion
   }
 }
 
